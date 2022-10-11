6d176895d7c31337bfe421c74044c327876dced1
Patrolin <patrik@balas.cz>
2022-10-11 23:37:01 +0200

    fix datetime

--- qLib/time_.py
+++ qLib/time_.py
@@ -10,78 +10,6 @@ def perf_time():
 def time():
     return _time_ns()
 
-def _isLeapYear(year):
-    return ((year % 4) == 0) - ((year % 100) == 0) + ((year % 400) == 0)
-
-def _daysBeforeYear(yearMinusOne):
-    return yearMinusOne*365 + (yearMinusOne//4) - (yearMinusOne//100) + (yearMinusOne//400)
-
-def _daysBeforeMonth(_yearMinusOne, _monthMinusOne):
-    yearMinusOne = _yearMinusOne + (_monthMinusOne//12)
-    yearDays = _daysBeforeYear(yearMinusOne)
-    monthMinusOne = _monthMinusOne % 12
-    k = 30 + 0.5/1 + 0.5/8
-    monthDays = floor(monthMinusOne*k + 0.5) - (monthMinusOne > 1) * (2 - _isLeapYear(yearMinusOne + 1))
-    return yearDays + monthDays
-
-def _dateToGregorianSecond(year, month=1, day=1, hour=0, minute=0, second=0, ms=0):
-    return (_daysBeforeMonth(year - 1, month - 1) + (day-1)) * 86400 + hour*3600 + minute*60 + second + ms/1000
-
-def _gregorianSecondToDate(gregorianSecond):
-    # TODO: fix 13/00 at the end of every year
-    AVERAGE_DAYS_PER_YEAR = 365 + 1/4 - 1/100 + 1/400
-    acc_seconds = gregorianSecond
-    yearMinusOneOrTwo = floor(acc_seconds / 86400 / AVERAGE_DAYS_PER_YEAR)
-    yearMinusOne = yearMinusOneOrTwo + (_daysBeforeYear(yearMinusOneOrTwo + 1) * 86400 <= gregorianSecond)
-    acc_seconds -= _daysBeforeYear(yearMinusOne) * 86400
-    accDays = acc_seconds / 86400
-    print("days", accDays, yearMinusOne, _isLeapYear(yearMinusOne + 1))
-    monthMinusOne = (accDays >= 31) * floor((accDays + (2 - _isLeapYear(yearMinusOne + 1))) / 30.5) # TODO: check this
-    acc_seconds = gregorianSecond - _daysBeforeMonth(yearMinusOne, monthMinusOne) * 86400
-    dayMinusOne = floor(acc_seconds / 86400)
-    acc_seconds -= dayMinusOne * 86400
-    hour = floor(acc_seconds / 3600)
-    acc_seconds -= hour * 3600
-    minute = floor(acc_seconds / 60)
-    acc_seconds -= minute * 60
-    second = floor(acc_seconds)
-    acc_seconds -= second
-    ms = floor(acc_seconds * 1000)
-    print(acc_seconds * 1000)
-    return [yearMinusOne + 1, monthMinusOne + 1, dayMinusOne + 1, hour, minute, second, ms, dayMinusOne % 7] # TODO: check weekday
-
-# TODO: https://www.timeanddate.com/time/zones/
-# TODO: Locale
-#GREGORIAN_CALENDAR_EPOCH_YEAR = 1583 # Gregorian calendar Epoch = October 1582
-#TAI_EPOCH_YEAR = 1958
-# POSIX doesn't define what a second is, so people just slow down/speed up time
-# whenever a leap seconds happens to make computations easier,
-# therefore time is always broken past the previous leap second
-POSIX_EPOCH_YEAR = 1970
-UTC_EPOCH_YEAR = 1972 # UTC = TAI + (N leap seconds) + (10 unlisted leap seconds)
-
-class DateTime:
-    def __init__(self, year, month=1, day=1, hour=0, minute=0, second=0, ms=0):
-        self.gregorianSecond = _dateToGregorianSecond(year, month, day, hour, minute, second, ms)
-
-    def toParts(self):
-        return _gregorianSecondToDate(self.gregorianSecond)
-
-    def toUTCTimeStamp(self):
-        return self.gregorianSecond - _dateToGregorianSecond(POSIX_EPOCH_YEAR)
-
-    def __repr__(self):
-        [year, month, day, h, m, s, ms, weekday] = self.toParts()
-        dateString = f"{year:04}-{month:02}-{day:02}"
-        timeString = f"T{h:02}:{m:02}:{s:02}" if (year >= UTC_EPOCH_YEAR) else ""
-        msString = f".{floor(ms):03}" if ms > 0 else ""
-        timezoneString = "Z" if (year > UTC_EPOCH_YEAR) else ""
-        return f"{dateString}{timeString}{msString}{timezoneString}"
-
-    def toHistoricString(self):
-        [year, *_] = self.toParts()
-        return f"{year} AD" if (year > 0) else f"{1-year} BC"
-
 NS_PER_US = 1_000
 NS_PER_MS = NS_PER_US * 1_000
 NS_PER_S = NS_PER_MS * 1_000
@@ -131,3 +59,73 @@ def tprint_time(value: int) -> str:
     if us > 0: acc_str += f"{us} us "
     if ns > 0: acc_str += f"{ns} ns "
     return acc_str[:-1]
+
+def _isLeapYear(year):
+    return ((year % 4) == 0) - ((year % 100) == 0) + ((year % 400) == 0)
+
+def _daysBeforeYear(yearMinusOne):
+    return yearMinusOne*365 + (yearMinusOne//4) - (yearMinusOne//100) + (yearMinusOne//400)
+
+def _daysBeforeYearMonth(yearMinusOne, monthMinusOne):
+    acc_yearMinusOne = yearMinusOne + (monthMinusOne//12)
+    acc_monthMinusOne = monthMinusOne % 12
+    k = 30 + 0.5/1 + 0.5/8
+    monthDays = floor(acc_monthMinusOne*k + 0.5) - (acc_monthMinusOne > 1) * (2 - _isLeapYear(acc_yearMinusOne + 1))
+    return _daysBeforeYear(acc_yearMinusOne) + monthDays
+
+def _utcToGregorian(year, month=1, day=1, hour=0, minute=0, second=0, ms=0):
+    return (_daysBeforeYearMonth(year - 1, month - 1) + (day-1)) * 86400 + hour*3600 + minute*60 + second + ms/1000
+
+_AVERAGE_DAYS_PER_YEAR = 365 + 1/4 - 1/100 + 1/400
+
+def _gregorianToUtc(gregorianSecond): # TODO: use ns
+    acc_seconds = gregorianSecond
+    yearMinusOneOrTwo = floor(acc_seconds / 86400 / _AVERAGE_DAYS_PER_YEAR)
+    yearMinusOne = yearMinusOneOrTwo + (_daysBeforeYear(yearMinusOneOrTwo + 1) * 86400 <= gregorianSecond)
+    acc_seconds -= _daysBeforeYear(yearMinusOne) * 86400
+    days = acc_seconds // 86400
+    monthMinusOne = floor((days + (2 - _isLeapYear(yearMinusOne + 1)) * (days > 40) - 0.5 * (days > 240)) / 30.5)
+    acc_seconds = gregorianSecond - _daysBeforeYearMonth(yearMinusOne, monthMinusOne) * 86400
+    dayMinusOne, acc_seconds = divmod(acc_seconds, 86400)
+    hour, acc_seconds = divmod(acc_seconds, 3600)
+    minute, acc_seconds = divmod(acc_seconds, 60)
+    second = floor(acc_seconds)
+    acc_seconds -= second
+    ms = floor(acc_seconds * 1000)
+    weekday = (_daysBeforeYearMonth(yearMinusOne, monthMinusOne) + dayMinusOne + 1) % 7
+    return [yearMinusOne + 1, monthMinusOne + 1, dayMinusOne + 1, hour, minute, second, ms, weekday]
+
+# TODO: https://www.timeanddate.com/time/zones/
+# TODO: Locale
+# POSIX doesn't define what a second is, so people just slow down/speed up time
+# whenever a leap seconds happens to make computations easier,
+# therefore time is always broken across leap seconds
+#JULIAN_CALENDAR_EPOCH_YEAR = -44 # 1 January AUC 709
+#GREGORIAN_CALENDAR_EPOCH_YEAR = 1583 # October 1582
+TAI_EPOCH_YEAR = 1958 # International Atomic Time
+#UTC_V1_EPOCH_YEAR = 1961 # UTC_V1 = TAI + x fractional leap seconds
+POSIX_EPOCH_YEAR = 1970 # same as UTC
+UTC_EPOCH_YEAR = 1972 # UTC = TAI + n integer leap seconds
+#GPS_EPOCH = 1980 # set to UTC in 1980, updated as atomic
+
+class DateTime:
+    def __init__(self, year, month=1, day=1, hour=0, minute=0, second=0, ms=0):
+        self.gregorianSecond = _utcToGregorian(year, month, day, hour, minute, second, ms)
+
+    def toParts(self):
+        return _gregorianToUtc(self.gregorianSecond)
+
+    def toUTCTimeStamp(self):
+        return self.gregorianSecond - _utcToGregorian(POSIX_EPOCH_YEAR)
+
+    def __repr__(self):
+        [year, month, day, h, m, s, ms, weekday] = self.toParts()
+        dateString = f"{year:04}-{month:02}-{day:02}"
+        timeString = f"T{h:02}:{m:02}:{s:02}" if (year >= UTC_EPOCH_YEAR) else ""
+        msString = f".{floor(ms):03}" if ms > 0 else ""
+        timezoneString = "Z" if (year > UTC_EPOCH_YEAR) else ""
+        return f"{dateString}{timeString}{msString}{timezoneString}"
+
+    def toHistoricString(self):
+        [year, *_] = self.toParts()
+        return f"{year} AD" if (year > 0) else f"{1-year} BC"
--- tests/time_.py
+++ tests/time_.py
@@ -10,22 +10,25 @@ class DateTimeTest(NamedTuple):
 @test
 def testDateTime():
     tests = [
-        DateTimeTest([1], 0, (1, 1, 1, 0, 0, 0, 0, 0)),
-        DateTimeTest([1970], 62135596800, (1970, 1, 1, 0, 0, 0, 0, 0)),
+        DateTimeTest([1], 0, (1, 1, 1, 0, 0, 0, 0, 1)),
+        DateTimeTest([2], 86400 * 365, (2, 1, 1, 0, 0, 0, 0, 2)),
+        DateTimeTest([5], 86400 * (4*365 + 1), (5, 1, 1, 0, 0, 0, 0, 6)),
+        DateTimeTest([1970], 62135596800, (1970, 1, 1, 0, 0, 0, 0, 4)),
         DateTimeTest([2022, 5, 13, 23, 59, 59, 999], 63788083199.999, (2022, 5, 13, 23, 59, 59, 999, 5)),
-        DateTimeTest([2021, 2, 28, 1, 2, 3, 4], 63750070923.004, (2021, 2, 28, 1, 2, 3, 3, 6)), # TODO: remove ms
-        # end of year
-        DateTimeTest([1970, 12, 31, 0, 0, 0, 0], 62167046400, (1970, 12, 31, 0, 0, 0, 0, 6)),
-        DateTimeTest([1970, 12, 31 + 1, 0, 0, 0, 0], 62167132800, (1971, 1, 1, 0, 0, 0, 0, 0)),
-        DateTimeTest([1970, 12 + 1, 31, 0, 0, 0, 0], 62169724800, (1971, 1, 31, 0, 0, 0, 0, 2)),
-        DateTimeTest([1971, 1, 1 - 1, 0, 0, 0, 0], 62167046400, (1970, 12, 31, 0, 0, 0, 0, 6)),
-        DateTimeTest([1971, 1 - 1, 31, 0, 0, 0, 0], 62167046400, (1970, 12, 31, 0, 0, 0, 0, 6)),
-        DateTimeTest([1970, 12 + 1, 1 - 1, 23, 0, 0, 0], 62167129200, (1970, 13, 0, 23, 0, 0, 0, 0)),
+        DateTimeTest([2021, 2, 28, 1, 2, 3, 4], 63750070923.004, (2021, 2, 28, 1, 2, 3, 3, 0)),
+        # wrapping
+        DateTimeTest([1971, 1, 1, 0, 0, 0, 0], 62167132800, (1971, 1, 1, 0, 0, 0, 0, 5)),
+        DateTimeTest([1970, 12, 31 + 1, 0, 0, 0, 0], 62167132800, (1971, 1, 1, 0, 0, 0, 0, 5)),
+        DateTimeTest([1971, 1, 31, 0, 0, 0, 0], 62169724800, (1971, 1, 31, 0, 0, 0, 0, 0)),
+        DateTimeTest([1970, 12 + 1, 31, 0, 0, 0, 0], 62169724800, (1971, 1, 31, 0, 0, 0, 0, 0)),
+        DateTimeTest([1970, 12, 31, 0, 0, 0, 0], 62167046400, (1970, 12, 31, 0, 0, 0, 0, 4)),
+        DateTimeTest([1971, 1, 1 - 1, 0, 0, 0, 0], 62167046400, (1970, 12, 31, 0, 0, 0, 0, 4)),
+        DateTimeTest([1971, 1 - 1, 31, 0, 0, 0, 0], 62167046400, (1970, 12, 31, 0, 0, 0, 0, 4)),
+        DateTimeTest([1970, 12 + 1, 1 - 1, 23, 0, 0, 0], 62167129200, (1970, 12, 31, 23, 0, 0, 0, 4)),
     ]
     for (date, gregorianSecond, expectedParts) in tests:
-        print(date)
-        assert_equals(DateTime(*date).gregorianSecond, gregorianSecond)
         assert_equals(DateTime(*date).toParts(), list(expectedParts))
+        assert_equals(DateTime(*date).gregorianSecond, gregorianSecond)
 
 if __name__ == "__main__":
     run_tests()
