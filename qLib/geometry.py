a203f72a1d50819b8ca4181a0210d53fcd9778a5
Patrolin <patrik@balas.cz>
2022-10-13 19:21:48 +0200

    multivector ops

--- qLib/geometry.py
+++ qLib/geometry.py
@@ -3,7 +3,7 @@ from qLib.collections_ import reduce
 from qLib.serialize.serialize_int import parseInt
 from .tests import assert_equals, assert_greater_than_equals, assert_less_than_equals, assert_never
 
-_INT_BASE = 16
+_INT_BASE = 10
 MAX_PRINT_BLADES = 2**16
 
 def GAlgebra(positive: int, negative=0, zero=0, start_with_zero=False, signs: list[str] = []):
@@ -22,24 +22,33 @@ def GAlgebra(positive: int, negative=0, zero=0, start_with_zero=False, signs: li
     blade_count = 2**(positive + negative + zero)
     assert_less_than_equals(log_blade_count, _INT_BASE)
 
-    def parse_blade_normalized(s: str, i: int) -> tuple["Blade", int]:
-        acc, i = parseInt(s, i) # TODO: signed int
-        if i <= 0: acc = 1
+    def parse_blade_normalized(s: str) -> tuple["Blade", int]:
+        i = 0
+        acc, j = parseInt(s[i:], _INT_BASE) # TODO: signed int
+        if j <= 0:
+            acc = 1
+        else:
+            i += j
+        j = 0
         # TODO: str_value = parse_op()?
         acc_str = ""
-        for v in s[1:]:
-            acc_str += v
-            i = len(acc_str) - 1
-            while i > 0:
-                if acc_str[i - 1] <= acc_str[i]: break
-                acc *= -1
-                tmp = acc_str[i] + acc_str[i - 1]
-                acc_str = acc_str[:i - 1] + tmp + acc_str[i + 1:]
-                i -= 1
-            if i - 1 >= 0 and acc_str[i] == acc_str[i - 1]:
-                acc *= bases[int(acc_str[i], _INT_BASE) - min_basis]
-                acc_str = acc_str[:i - 1] + acc_str[i + 1:]
-        return Blade(acc, "", f"e{acc_str}" if acc_str else "1"), len(s)
+        if i < len(s) and s[i] == "e":
+            i += 1
+            j = parseInt(s[i:], _INT_BASE)[1]
+            if j > 0:
+                for v in s[i:i + j]:
+                    acc_str += v
+                    k = len(acc_str) - 1
+                    while k > 0:
+                        if acc_str[k - 1] <= acc_str[k]: break
+                        acc *= -1
+                        tmp = acc_str[k] + acc_str[k - 1]
+                        acc_str = acc_str[:k - 1] + tmp + acc_str[k + 1:]
+                        k -= 1
+                    if k - 1 >= 0 and acc_str[k] == acc_str[k - 1]:
+                        acc *= bases[int(acc_str[k], _INT_BASE) - min_basis]
+                        acc_str = acc_str[:k - 1] + acc_str[k + 1:]
+        return Blade(acc, "", f"e{acc_str}" if acc_str else "1"), i + j
 
     def genBlades():
         acc: list[str] = []
@@ -67,7 +76,7 @@ def GAlgebra(positive: int, negative=0, zero=0, start_with_zero=False, signs: li
 
     def denormalize_blade(normalized_blade: "Blade") -> "Blade":
         unit = mask_to_unit[bladeMask(normalized_blade.name)]
-        normalized_unit, i = parse_blade_normalized(unit.name, 0)
+        normalized_unit, i = parse_blade_normalized(unit.name)
         assert_greater_than_equals(i, 1)
         return Blade(normalized_blade.value * normalized_unit.value, normalized_blade.str_value, unit.name)
 
@@ -123,7 +132,7 @@ def GAlgebra(positive: int, negative=0, zero=0, start_with_zero=False, signs: li
 
         def __mul__(self, other: Union[int, float, "Blade"]) -> "Blade":
             if isinstance(other, Blade):
-                normalized_blade, i = parse_blade_normalized(f"e{self.name[1:]}{other.name[1:]}", 0)
+                normalized_blade, i = parse_blade_normalized(f"e{self.name[1:]}{other.name[1:]}")
                 assert_greater_than_equals(i, 0)
                 normalized_blade.value *= self.value * other.value
                 normalized_blade.str_value = self.str_value + other.str_value
@@ -166,36 +175,97 @@ def GAlgebra(positive: int, negative=0, zero=0, start_with_zero=False, signs: li
     assert_equals(len(mask_to_unit), blade_count)
 
     class Multivector:
-        values: list[Blade] = []
+        values: list[Blade]
 
-        def add(self, blade: Blade):
+        def __init__(self):
+            self.values = []
+
+        def __repr__(self):
+            def _print_blade(i: int, blade: Blade):
+                sign = "" if (i == 0) and (blade.value >= 0) \
+                    else ("-" if (i == 0)
+                    else (" + " if (blade.value >= 0) else " - "))
+                return sign + repr(blade).removeprefix("+").removeprefix("-")
+
+            return f"({''.join(_print_blade(i, v) for i, v in enumerate(self.values))})"
+
+        def _add(self, blade: Blade):
+            if blade.value == 0: return
             for v in self.values:
                 if (v.str_value == blade.str_value) and (v.name == blade.name):
                     v.value += blade.value
                     return
             self.values.append(blade)
 
+        def __add__(self, other: "Multivector"):
+            acc = Multivector()
+            acc.values = self.values.copy()
+            for blade in other.values:
+                acc._add(blade)
+            return acc
+
+        def _map(self, key: Callable[[Blade], Blade]):
+            acc = Multivector()
+            for v in self.values:
+                acc._add(key(v))
+            return acc
+
+        def __invert__(self):
+            return self._map(lambda v: ~v)
+
+        def dual(self): # right complement
+            return self._map(lambda v: v.dual())
+
+        def undual(self): # left complement
+            return self._map(lambda v: v.undual())
+
+        def _starmap(self, other: "Multivector", key: Callable[[Blade, Blade], Blade]):
+            acc = Multivector()
+            for a in self.values:
+                for b in other.values:
+                    acc._add(key(a, b))
+            return acc
+
+        def __mul__(self, other: "Multivector"):
+            return self._starmap(other, lambda a, b: a * b)
+
+        def dot(self, other: "Multivector"):
+            return self._starmap(other, lambda a, b: Blade(a.dot(b), "", "1"))
+
+        def inner(self, other: "Multivector"):
+            return self._starmap(other, lambda a, b: a.inner(b))
+
+        def left_contraction(self, other: "Multivector"):
+            return self._starmap(other, lambda a, b: a.left_contraction(b))
+
+        def __xor__(self, other: "Multivector"): # wedge product
+            return self._starmap(other, lambda a, b: a ^ b)
+
+        def __and__(self, other: "Multivector"): # antiwedge (regressive) product
+            return self._starmap(other, lambda a, b: a & b)
+
     class GAlgebra:
         name = generic_name = f"G_{positive},{negative},{zero}"
         blades: list[Blade]
 
         @staticmethod
-        def parse_blade(s: str, i: int) -> tuple[Blade, int]:
-            blade, i = parse_blade_normalized(s, i)
+        def parse_blade(s: str) -> tuple[Blade, int]:
+            blade, i = parse_blade_normalized(s)
             return denormalize_blade(blade), i
 
         @staticmethod
-        def parse_multivector(s: str, i: int) -> tuple[Multivector, int]:
+        def parse_multivector(s: str) -> tuple[Multivector, int]:
             acc = Multivector()
-            i = (s[0] == "(")
+            i = int(s[0] == "(")
             while i < len(s):
-                blade, j = parse_blade_normalized(s, i)
-                if j <= i: break
-                acc.add(blade)
-                i = j
                 # TODO: whitespace
                 if i < len(s) and s[i] == "+": i += 1
                 # TODO: whitespace
+                blade, j = parse_blade_normalized(s[i:])
+                j += i
+                if j <= i: break
+                acc._add(blade)
+                i = j
             return acc, i
 
         @staticmethod
@@ -248,10 +318,11 @@ HYPERBOLIC_NUMBERS = GAlgebra(1, 0, 0)
 COMPLEX_NUMBERS = GAlgebra(0, 1, 0)
 DUAL_NUMBERS = GAlgebra(0, 0, 1)
 CONIC_ALGEBRA = GAlgebra(5, 3, 0)
-QUADRIC_ALGEBRA = GAlgebra(9, 6, 0)
+#QUADRIC_ALGEBRA = GAlgebra(9, 6, 0)
 VGA_2D = GAlgebra(2, 0, 0)
 VGA_3D = GAlgebra(3, 0, 0)
 PGA_2D = GAlgebra(2, 0, 1, start_with_zero=True, signs=["e20"])
 PGA_3D = GAlgebra(3, 0, 1, start_with_zero=True, signs=["e31", "e021", "e032"])
+PGA_4D = GAlgebra(4, 0, 1, start_with_zero=True)
 CGA_2D = GAlgebra(3, 1, 0)
 CGA_3D = GAlgebra(4, 1, 0)
--- tests/geometry.py
+++ tests/geometry.py
@@ -22,11 +22,21 @@ def testPGA_2D():
         print("-(A.dual() commutator B).undual()")
         PGA_2D.print_table(lambda a, b: -a.dual().commutator(b).undual())
 
-@test
+#@test
 def testWedge():
-    for G in (PGA_2D, PGA_3D, CGA_2D, CGA_3D):
+    for G in (PGA_2D, PGA_3D, PGA_4D, CGA_2D, CGA_3D):
         G.assert_equals(lambda a, b: a & b, lambda a, b: (a.dual() ^ b.dual()).undual())
         G.assert_equals(lambda a, b: a ^ b, lambda a, b: (a.dual() & b.dual()).undual())
+
+@test
+def testMultivector():
+    for G in (PGA_2D, PGA_3D):
+        G.print_name()
+        A = G.parse_multivector("1+2e1+e012")[0]
+        B = G.parse_multivector("1+e1")[0]
+        print(A)
+        print(B)
+        print(A * B)
 
 if __name__ == "__main__":
     run_tests()
