d6f1a2d17f0a255fa9b3d42ec70cd6a7b044621f
Patrolin <patrik@balas.cz>
2023-03-25 00:50:20 +0100

    cleanup

--- qclib/build.bat
+++ qclib/build.bat
@@ -1,12 +1,14 @@
 @echo off
+set my_files=.\qclib\main.cpp -o .\qclib\out\qclib.exe
+set my_args=-nostdlib++ -no-integrated-cpp -z /subsystem:windows -z Kernel32.lib -z User32.lib -g -gcodeview
+::clang %my_files% %my_args% -nostdlib -z /subsystem:windows -z /entry:main
 :: 14336 B
-::clang .\qclib\main.cpp -o .\qclib\out\qclib.exe -nostdlib -z /subsystem:windows -z /entry:main -nostdlib++ -no-integrated-cpp -z Kernel32.lib -z User32.lib -g -gcodeview
-:: <signal.h> depends on stdlib
-:: TODO: fork signal.h so that it doesn't depend on stdlib
+:: but <signal.h> depends on stdlib
+:: we want a fork of <signal.h> that doesn't depend on stdlib
+:: except Microsoft does not support this anymore, the closest we have is a chinese reverse engineer of <ucrt/corecrt_internal.h>
 
-:: With these removed: -nostdlib -z /subsystem:windows -z /entry:main
+clang %my_files% %my_args%
 :: 567808 B
-clang .\qclib\main.cpp -o .\qclib\out\qclib.exe -nostdlib++ -no-integrated-cpp -z /subsystem:windows -z Kernel32.lib -z User32.lib -g -gcodeview
 
-:: TODO: libctiny.lib?
-:: TODO: check .MAP file to find space optimizations
+:: (if you didn't need stdlib, you could try using something like libctiny.lib
+:: and check linker .MAP file to find even more space optimizations)
